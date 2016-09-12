0cb821e9902082bd2959a1859aa90314fa9ed127
Jerome Leclanche <jerome@leclan.ch>
2016-09-13 01:30:07 +0300

    Add DEBUG mode for PowerlogS3Protocol

--- reports/protocols.py
+++ reports/protocols.py
@@ -1,6 +1,7 @@
 """
 Reusable MRJob protocols to give MRJob scripts access to HSReplay.net objects.
 """
+
 import boto3
 from gzip import decompress
 from io import StringIO
@@ -13,36 +14,41 @@ S3 = boto3.client("s3")
 class HSReplayS3Protocol:
 	def read(self, line):
 		# Each line should be to a hsreplay.xml file
-		try:
-			bucket, sep, key = line.decode("utf8").partition(":")
-			obj = S3.get_object(Bucket=bucket, Key=key)
-			xml_str = decompress(obj["Body"].read()).decode("utf8")
+		bucket, sep, key = line.decode("utf8").partition(":")
+		obj = S3.get_object(Bucket=bucket, Key=key)
+		xml_str = decompress(obj["Body"].read()).decode("utf8")
 
-			shortid = key[25:-13]
-			replay = HSReplayDocument.from_xml_file(StringIO(xml_str))
-		except Exception as e:
-			# TODO: Find out why from_xml_file fails on some replays
-			return None, None
-		else:
-			return shortid, replay
+		shortid = key[25:-13]
+		replay = HSReplayDocument.from_xml_file(StringIO(xml_str))
+
+		return shortid, replay
 
 
 class PowerlogS3Protocol:
+	DEBUG = True
+
+	def read_s3(self, bucket, key):
+		obj = S3.get_object(Bucket=bucket, Key=key)
+		log_str = decompress(obj["Body"].read()).decode("utf8")
+		out = StringIO()
+		out.write(log_str)
+		out.seek(0)
+
+		return out
+
 	def read(self, line):
-		try:
-			# Each line should be to a power.log
-			bucket, sep, key = line.decode("utf8").partition(":")
-			if bucket == "local":
-				out = open(key)
-			else:
-				obj = S3.get_object(Bucket=bucket, Key=key)
-				log_str = decompress(obj["Body"].read()).decode("utf8")
+		# Each line should be to a power.log
+		bucket, sep, key = line.decode("utf8").partition(":")
 
-				out = StringIO()
-				out.write(log_str)
-				out.seek(0)
-
-		except Exception as e:
-			return None, None
+		if bucket == "local":
+			out = open(key)
 		else:
-			return line, out
+			try:
+				out = self.read_s3(bucket, key)
+			except Exception:
+				if self.DEBUG:
+					raise
+				else:
+					return None, ""
+
+		return line, out
