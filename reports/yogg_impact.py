851a8a64a6fc94a2e95ba36c97ca31ae7ddae0a1
Alexander Zeier <alex@hearthsim.net>
2016-10-15 01:05:58 +0200

    update yogg_impact for new replay exporter

--- reports/yogg_impact.py
+++ reports/yogg_impact.py
@@ -7,100 +7,69 @@ Input: A sample of games in which Yogg-Saron may have been played (eg. in deck)
 Format: Power.log
 """
 
-from collections import defaultdict
-from hearthstone.enums import GameTag, BlockType, PlayState
-from hearthstone.hslog.watcher import LogWatcher
+from hearthstone.enums import GameTag, BlockType, PlayState, PowerType
+from hearthstone.hslog.export import EntityTreeExporter
 from mrjob.job import MRJob
-from protocols import PowerlogS3Protocol
+from protocols import HSReplayS3Protocol
 
-
-class YoggEventWatcher(LogWatcher):
+class YoggExporter(EntityTreeExporter):
 	YOGG_SARON = "OG_134"
 
-	def __init__(self):
-		super().__init__()
-		self.yogg_events = []
+	def __init__(self, packet_tree):
+		super().__init__(packet_tree)
+		self.events = []
 
-	def on_block(self, action):
-		if hasattr(action, "type") and action.type == BlockType.PLAY:
-			if action.entity.card_id == self.YOGG_SARON:
-				player = self.current_game.current_player.id
-				turn = self.current_game.tags.get(GameTag.TURN, 0)
-				self.yogg_events.append((player, turn))
+	def handle_block(self, block):
+		if block.type == BlockType.PLAY:
+			entity = self.game.find_entity_by_id(block.entity)
+			if entity.card_id == self.YOGG_SARON:
+				self.handle_played_yogg(block, entity)
+		super().handle_block(block)
 
+	def is_block_type(self, block, block_type):
+		return block.power_type == PowerType.BLOCK_START and block.type == block_type
+
+	def get_played_sub_cards(self, block):
+		for packet in block.packets:
+			if self.is_block_type(packet, BlockType.POWER):
+				for sub_packet in packet.packets:
+					if self.is_block_type(sub_packet, BlockType.PLAY):
+						entity = self.game.find_entity_by_id(sub_packet.entity)
+						if entity.card_id:
+							yield entity.card_id
+
+	def handle_played_yogg(self, block, entity):
+		controller = entity.controller
+		if controller:
+			turn = self.game.tags.get(GameTag.TURN)
+			played_cards = self.get_played_sub_cards(block)
+			self.events.append((controller.player_id, turn, played_cards))
 
 class Job(MRJob):
-	INPUT_PROTOCOL = PowerlogS3Protocol
+	INPUT_PROTOCOL = HSReplayS3Protocol
 
-	def mapper(self, line, log_fp):
-		if not log_fp:
-			return
-		watcher = YoggEventWatcher()
-
-		try:
-			watcher.read(log_fp)
-		except Exception as e:
+	def mapper(self, line, replay):
+		if not replay:
 			return
 
-		packet_tree = watcher.games[0]
-		game = packet_tree.game
+		packet_tree = replay.to_packet_tree()[0]
+		exporter = packet_tree.export(YoggExporter)
+		game = exporter.game
 
 		player1, player2 = game.players
-		state1 = player1.tags[GameTag.PLAYSTATE]
-		# state2 = player2.tags[GameTag.PLAYSTATE]
+		state1 = player1.tags.get(GameTag.PLAYSTATE, 0)
 
 		total_num_turns = game.tags.get(GameTag.TURN, 0)
 
 		# This does not handle co-op or ties
-		winning_player_id = player1.id if state1 == PlayState.WON else player2.id
+		winner = player1.player_id if state1 == PlayState.WON else player2.player_id
 
-		for player_id_that_played_yogg, turn_yogg_played in watcher.yogg_events:
-			num_turns_made_after_yogg = total_num_turns - turn_yogg_played
-			yogg_controller_won = player_id_that_played_yogg == winning_player_id
-
-			if yogg_controller_won:
-				line = "%s,%s,1" % (num_turns_made_after_yogg, "YOGG_CONTROLLER_WON")
-			else:
-				line = "%s,%s,1" % (num_turns_made_after_yogg, "YOGG_CONTROLLER_LOST")
+		for player_id, turn, cards_played in exporter.events:
+			turns_post_yogg = total_num_turns - turn
+			result = "WON" if player_id == winner else "LOST"
+			line = "%s,%s,%s" % (turns_post_yogg, "|".join(cards_played), result)
 
 			yield None, line
-
-	def combiner_init(self):
-		self.DATA_SET = defaultdict(lambda: defaultdict(int))
-
-	def combiner(self, key, yogg_events):
-		for event_str in yogg_events:
-			num_turns, outcome, count = event_str.split(",")
-			self.DATA_SET[num_turns][outcome] += int(count)
-
-	def combiner_final(self):
-		for turn in sorted(self.DATA_SET.keys(), key=lambda k: int(k)):
-			data = self.DATA_SET[turn]
-			wins = [str(turn), "YOGG_CONTROLLER_WON", str(data["YOGG_CONTROLLER_WON"])]
-			losses = [str(turn), "YOGG_CONTROLLER_LOST", str(data["YOGG_CONTROLLER_LOST"])]
-
-			yield None, ",".join(wins)
-			yield None, ",".join(losses)
-
-	def reducer_init(self):
-		self.DATA_SET = defaultdict(lambda: defaultdict(int))
-
-	def reducer(self, key, yogg_events):
-		for event_str in yogg_events:
-			num_turns, outcome, count = event_str.split(",")
-			self.DATA_SET[num_turns][outcome] += int(count)
-
-	def reducer_final(self):
-		COLUMNS = ["TURNS_SINCE_YOGG_PLAYED", "YOGG_CONTROLLER_WON", "YOGG_CONTROLLER_LOST"]
-		yield None, ",".join(COLUMNS)
-
-		for num_turns in sorted(self.DATA_SET.keys(), key=lambda k: int(k)):
-			row_data = self.DATA_SET[num_turns]
-			wins = row_data["YOGG_CONTROLLER_WON"]
-			losses = row_data["YOGG_CONTROLLER_LOST"]
-
-			row_result = [str(num_turns), str(wins), str(losses)]
-			yield None, ",".join(row_result)
 
 
 if __name__ == "__main__":
